ef1ad07b942e5831f287a063bd4d5b7727239ba8
jrso15 <janilleolegario@gmail.com>
2022-08-21 18:18:05 +0800

    getstaticprops for fetching data

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,19 @@ import Head from "../components/Head";
 import Header from "../components/header";
 import ProductList from "../components/ProductList";
 import styles from "../styles/Home.module.scss";
+import { getProductList } from "../pages/api/decathlon-api";
 
-const Home = () => {
+export const getStaticProps = async () => {
+  let productList = await getProductList();
+
+  return {
+    props: {
+      productList,
+    },
+  };
+};
+
+const Home = ({ productList }) => {
   return (
     <div className={styles.container}>
       <Head title="Decathlon Sports Philippines | Buy Sports Products Online Now..." />
@@ -11,7 +22,7 @@ const Home = () => {
       <Header />
 
       <main className={styles.main}>
-        <ProductList />
+        <ProductList productList={productList} />
       </main>
     </div>
   );
