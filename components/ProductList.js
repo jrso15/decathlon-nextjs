f2d1bd9063dfe0953c4cce93363656d14c7bc1bb
jrso15 <janilleolegario@gmail.com>
2022-08-21 18:18:45 +0800

    removed client side rendering, add link out for dynamic routes

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { useState, useEffect } from "react";
 import { getProductList } from "../pages/api/decathlon-api";
 import styles from "../styles/ProductList.module.scss";
@@ -6,41 +7,32 @@ import styles from "../styles/ProductList.module.scss";
 const loader = ({ src, width, quality }) => {
   return `${src}?w=${width}&q=${quality || 75}`;
 };
-const ProductList = () => {
-  const [productData, setProductData] = useState([]);
-
-  const fetchData = async () => {
-    let productList = await getProductList();
-    setProductData(productList);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  console.log("data", productData);
+const ProductList = ({ productList }) => {
+  console.log("listDDddd", productList);
 
   return (
     <div className={styles.productListContainer}>
-      {productData.records?.map((product) => (
-        <div className={styles.productListContainer__item} key={product.id}>
-          <div className={styles.image_wrapper}>
-            <Image
-              loader={loader}
-              src={product.fields.pictures[0].url}
-              alt="logo"
-              width={150}
-              height={150}
-            />
-          </div>
-          <div className={styles.product_details}>
-            <div className={styles.price}>
-              <p>{product.fields.Price}</p>
+      {productList.records?.map((product) => (
+        <Link href={`product/${product.id}`} key={product.id}>
+          <div className={styles.productListContainer__item}>
+            <div className={styles.image_wrapper}>
+              <Image
+                loader={loader}
+                src={product.fields.pictures[0].url}
+                alt="logo"
+                width={150}
+                height={150}
+              />
             </div>
-            <h4>{product.fields.Brand}</h4>
-            <h3>{product.fields.Name}</h3>
+            <div className={styles.product_details}>
+              <div className={styles.price}>
+                <p>{product.fields.Price}</p>
+              </div>
+              <h4>{product.fields.Brand}</h4>
+              <h3>{product.fields.Name}</h3>
+            </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
