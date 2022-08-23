df10de65b92f682b715e7ffde8cdcf2dcffc6a65
jrso15 <janilleolegario@gmail.com>
2022-08-23 18:29:17 +0800

    add checking for api

--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -9,29 +9,33 @@ const ProductList = ({ productList }) => {
 
   return (
     <div className={styles.productListContainer}>
-      {productList?.records?.map((product) => (
-        <Link href={`product/${product.id}`} key={product.id}>
-          <div className={styles.productListContainer__item}>
-            <div className={styles.image_wrapper}>
-              <Image
-                loader={loader}
-                src={product?.fields.pictures[0].url}
-                alt={product.fields.Name}
-                width={150}
-                height={150}
-              />
-            </div>
-
-            <div className={styles.details}>
-              <div className={styles.price}>
-                <p>PHP {product.fields.Price}</p>
+      {productList && productList.records && productList.records.length > 0 ? (
+        productList.records.map((product) => (
+          <Link href={`product/${product.id}`} key={product.id}>
+            <div className={styles.productListContainer__item}>
+              <div className={styles.image_wrapper}>
+                <Image
+                  loader={loader}
+                  src={product.fields.pictures[0].url}
+                  alt={product.fields.Name}
+                  width={150}
+                  height={150}
+                />
               </div>
-              <h3 className={styles.brand}>{product.fields.Brand}</h3>
-              <h2 className={styles.productName}>{product.fields.Name}</h2>
+
+              <div className={styles.details}>
+                <div className={styles.price}>
+                  <p>PHP {product.fields.Price}</p>
+                </div>
+                <h3 className={styles.brand}>{product.fields.Brand}</h3>
+                <h2 className={styles.productName}>{product.fields.Name}</h2>
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))
+      ) : (
+        <h3> No products available! </h3>
+      )}
     </div>
   );
 };
